385a99e4e779200ce2e8360b83779fc871ea6ad2
Karunasagar <72115529+Karunasagar-K@users.noreply.github.com>
2022-08-29 23:17:44 +0530

    Intro and Control Statements

---
+++ Intro and Control Statements.py
@@ -0,0 +1,10 @@
+age = 101
+
+if age>18 and age<=100:
+        print("You can vote in elections in India")
+elif age<0 or age>100:
+    print("Invalid age")
+elif age==0:
+    print("Baby")
+else:
+    print("You cannot vote in elections in India")
